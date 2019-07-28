c6420f206fb8ccb71861429abd84736e7fd1703e
PierreCapo <pierre.caporossi@gadz.org>
2019-07-28 16:40:40 +0200

    :tada: First commit

---
+++ precache-manifest.fcbdcec87f082f25a7ce8602acb2c019.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ef953a6cb2f74fb94a9bfb158c7e02",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8fe31c95e1f31ba54f0b.bundle.js"
+  },
+  {
+    "revision": "5bbad2beeaa2952b8802",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.8fe31c95e1f31ba54f0b.bundle.js"
+  },
+  {
+    "url": "vendors~main.8fe31c95e1f31ba54f0b.bundle.js"
+  }
+]);
